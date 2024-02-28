7127c35b44727cc578af1bcd981c72112df537f8
Uiratan Cavalcante <uiratan@gmail.com>
2024-02-28 18:50:10 -0300

    16.3. Stack Trace: interpretando e analisando exceções

--- excecoes_16/estoque/src/Principal.java
+++ excecoes_16/estoque/src/Principal.java
@@ -1,12 +1,32 @@
 import com.algaworks.estoque.Produto;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		var produto = new Produto("Apple Watch");
+//		var produto1 = new Produto(null);
 		produto.adicionarEstoque(10);
 		produto.ativar();
-		produto.retirarEstoque(15);
-		System.out.printf("Estoque: %d%n", produto.getQuantidadeEstoque());
+//		produto.retirarEstoque(15);
+//		System.out.printf("Estoque: %d%n", produto.getQuantidadeEstoque());
+
+		comprar(produto);
+	}
+
+	public static void comprar(Produto produto) {
+		var scanner = new Scanner(System.in);
+		System.out.print("Quantidade: ");
+		var quantidade = scanner.nextInt();
+
+		efetuarBaixaEstoque(produto, quantidade);
+		System.out.println("Compra realizada");
+	}
+
+	public static void efetuarBaixaEstoque(Produto produto, int quantidade) {
+		produto.retirarEstoque(quantidade);
+		System.out.printf("%d unidades retirada do estoque, Estoque atual: %d%n",
+				quantidade, produto.getQuantidadeEstoque());
 	}
 }
