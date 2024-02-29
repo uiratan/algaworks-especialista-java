2ae1bf9cc02fcea16139148946e16e4153449cb1
Uiratan Cavalcante <uiratan@gmail.com>
2024-02-29 07:32:55 -0300

    16.16. Usando a cláusula finally

--- excecoes_16/excecoes/src/Principal.java
+++ excecoes_16/excecoes/src/Principal.java
@@ -1,3 +1,4 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -6,14 +7,24 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Path arquivo = Path.of("/mnt/wsl/PHYSICALDRIVE1/java/algaworks/ej/excecoes_16/excecoes/teste.txt");
+		BufferedReader reader = null;
 
 		try {
-			Files.createFile(arquivo);
+			reader = Files.newBufferedReader(arquivo);
+			System.out.println(reader.readLine());
+
 		} catch (IOException e) {
 			System.out.println("Erro ao criar arquivo: " + e.getMessage());
-			e.printStackTrace();
+
+		} finally {
+
+			try {
+				reader.close();
+			} catch (IOException e) {
+				System.out.println("Erro fechando leitor de arquivo");
+			}
+
 		}
 
-		System.out.println(arquivo);
 	}
 }
