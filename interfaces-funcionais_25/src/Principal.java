2d242bbf0d7df2d2f43cc401c2e537f6b565718b
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-09 17:02:25 -0300

    25.2. Implementando Expressões Lambda

--- interfaces-funcionais_25/src/Principal.java
+++ interfaces-funcionais_25/src/Principal.java
@@ -11,15 +11,24 @@ public class Principal {
 		cadastroCliente.adicionar(new Cliente("Joaquina", 45));
 		cadastroCliente.adicionar(new Cliente("Josefina", 25));
 
-		var filtro = new Filtro<Cliente>() {
-			@Override
-			public boolean avaliar(Cliente cliente) {
-				return cliente.getIdade() > 40;
-			}
-		};
+//		var filtro = new Filtro<Cliente>() {
+//			@Override
+//			public boolean avaliar(Cliente cliente) {
+//				return cliente.getIdade() > 40;
+//			}
+//		};
 
 //		var clientes = cadastroCliente.getClientes();
-		var clientes = cadastroCliente.consultar(filtro);
+
+//		Filtro<Cliente> filtro = (Cliente cliente) -> {
+//			boolean resultado = cliente.getIdade() > 40;
+//			return resultado;
+//		};
+
+		var clientes = cadastroCliente.consultar((Cliente cliente) -> {
+			boolean resultado = cliente.getIdade() > 40;
+			return resultado;
+		});
 
 		for (Cliente cliente : clientes) {
 			System.out.printf("%s - %d%n", cliente.getNome(), cliente.getIdade());
