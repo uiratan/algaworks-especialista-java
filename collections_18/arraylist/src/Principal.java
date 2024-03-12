816720875124f846f48e96053466a52769b61c3e
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-11 22:51:16 -0300

    18.5. Iterando em lista com for tradicional

--- collections_18/arraylist/src/Principal.java
+++ collections_18/arraylist/src/Principal.java
@@ -1,6 +1,8 @@
 import com.algaworks.agencia.CadastroHotel;
 import com.algaworks.agencia.Hotel;
 
+import java.util.ArrayList;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -11,10 +13,16 @@ public class Principal {
 		cadastro.adicionar("Tivoli Ecoresort", "Praia do Forte/BA", 2000);
 		cadastro.adicionar("Mercure", "Uberlândia/MG", 400);
 
-//        System.out.println(cadastro.obterTodos());
+		ArrayList hoteis = cadastro.obterTodos();
+		imprimirHoteis(hoteis);
+	}
 
-		Hotel hotel = (Hotel) cadastro.obterTodos().get(1);
-		System.out.println(hotel.getNome());
+	private static void imprimirHoteis(ArrayList hoteis) {
+		for (int i = 0; i < hoteis.size(); i++) {
+			Hotel hotel = (Hotel) hoteis.get(i);
+			System.out.printf("%s (%s) -> %.2f%n", hotel.getNome(),
+					hotel.getCidade(), hotel.getPrecoDiaria());
+		}
 	}
 
 }
