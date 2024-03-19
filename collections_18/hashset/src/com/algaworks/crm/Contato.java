42936b33a17ee1f0662aa7d83fe268a653a5d464
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-19 18:19:33 -0300

    18.25. Testando a implementação de hashCode com HashSet

--- collections_18/hashset/src/com/algaworks/crm/Contato.java
+++ collections_18/hashset/src/com/algaworks/crm/Contato.java
@@ -51,6 +51,7 @@ public class Contato {
 
 	@Override
 	public boolean equals(Object o) {
+		System.out.printf("%s = %s%n", getEmail(), ((Contato) o).getEmail());
 		if (this == o) return true;
 		if (o == null || getClass() != o.getClass()) return false;
 		Contato contato = (Contato) o;
@@ -59,6 +60,6 @@ public class Contato {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(email);
+		return email.charAt(0);
 	}
 }
