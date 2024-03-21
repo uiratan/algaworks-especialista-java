3678922f3178e5a8c229e2b19701460bc701aa8c
Uiratan Cavalcante <uiratan@gmail.com>
2024-03-20 22:12:30 -0300

    18.34. Coleções imutáveis

---
+++ collections_18/colecoes-imutaveis/src/TesteOf.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class TesteOf {
+
+    public static void main(String[] args) {
+        List<Integer> numeros = List.of(1, 2);
+        System.out.println(numeros);
+    }
+
+}
