66106d397b38b211b67f9cc9c7a84ab69a24f678
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-02 12:38:02 -0300

    19.1. Usando enumerações à moda antiga

--- src/main/java/com/algaworks/c19enums/oldEnums/Principal.java
+++ src/main/java/com/algaworks/c19enums/oldEnums/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
     public static void main(String[] args) {
         Pedido pedido = new Pedido();
         pedido.setNomeCliente("João da Silva");
-        pedido.setStatus(Pedido.STATUS_ENTREGUE);
+        pedido.setStatus(Pedido.STATUS_CANCELADO);
         pedido.setOrigem(Pedido.ORIGEM_BALCAO);
 
         System.out.println(Pedido.getDescricaoStatus(pedido.getStatus()));
