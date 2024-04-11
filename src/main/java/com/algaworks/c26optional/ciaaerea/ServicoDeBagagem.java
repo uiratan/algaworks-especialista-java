f80654df682d64ca4859879b0b6fef2d2ea825e1
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-11 18:47:20 -0300

    26.4. Obtendo valor e lançando exceção com orElseThrow

--- src/main/java/com/algaworks/c26optional/ciaaerea/ServicoDeBagagem.java
+++ src/main/java/com/algaworks/c26optional/ciaaerea/ServicoDeBagagem.java
@@ -17,14 +17,13 @@ public class ServicoDeBagagem {
             throw new IllegalArgumentException("Quantidade de bagagens inválida");
         }
 
-        Optional<Reserva> reservaOptional = servicoDeReserva.buscar(codigoReserva);
-
-        if (reservaOptional.isPresent()) {
-            reservaOptional.get().adicionarBagagens(quantidadeBagagens);
-        } else {
-            throw new ReservaNaoEncontradaException("Reserva não encontrada");
-        }
+        // servicoDeReserva.buscar(codigoReserva)
+        //     .orElseThrow(ReservaNaoEncontradaException::new)
+        //     .adicionarBagagens(quantidadeBagagens);
         
+        servicoDeReserva.buscar(codigoReserva)
+            .orElseThrow(() -> new ReservaNaoEncontradaException("Reserva não encontrada: " + codigoReserva))
+            .adicionarBagagens(quantidadeBagagens);
     }
 
 }
