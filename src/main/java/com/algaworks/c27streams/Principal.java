48b69bb637e8462ed3999403db485c3bbbfbbd8e
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 11:52:17 -0300

    27.5. Executando ações intermediárias com o método Stream.peek

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -13,8 +13,10 @@ public class Principal {
         List<Produto> produtos = cadastroProduto.obterTodos();      
         
         produtos.stream()
-            // .filter(produto -> produto.temEstoque() && produto.isInativo())
+            .peek(produto -> produto.setNome(produto.getNome().toUpperCase()))
+            .peek(p -> System.out.println("Antes do temEstoque: " + p))
             .filter(Produto::temEstoque)
+            .peek(p -> System.out.println("Depois do temEstoque: " + p))
             .filter(Produto::isInativo)
             .forEach(produto -> {
                 System.out.println("Ativando: " + produto);
