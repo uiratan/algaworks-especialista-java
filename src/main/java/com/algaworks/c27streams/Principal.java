a5278bba1524d10c9a226ae6de6651e3b9f9d182
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 18:01:32 -0300

    27.18. Operações de redução especiais: min e max

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -1,11 +1,7 @@
 package com.algaworks.c27streams;
 
-import java.lang.reflect.Array;
-import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
 
 import com.algaworks.c27streams.estoque.CadastroProduto;
 import com.algaworks.c27streams.estoque.Produto;
@@ -15,25 +11,19 @@ public class Principal {
     public static void main(String[] args) {
         CadastroProduto cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();  
-
-        // int totalEstoque = produtos.stream()
+        
+        // produtos.stream()
+        //     .filter(Produto::temEstoque)
         //     .mapToInt(Produto::getQuantidade)
-        //     .sum();
-               
-        // System.out.println(totalEstoque);
+        //     .min()
+        //     .ifPresent(System.out::println);
 
-        // double mediaEstoque = produtos.stream()
-        //     .mapToInt(Produto::getQuantidade)
-        //     .average()
-        //     .orElseThrow(() -> new RuntimeException("Nenhum item encontrado"));
-           
-        // System.out.println(mediaEstoque);
-
-        long totalProdutosComEstoque = produtos.stream()
+        Produto produtoMaisBarato = produtos.stream()
             .filter(Produto::temEstoque)
-            .count();
-       
-        System.out.println(totalProdutosComEstoque);
+            .min(Comparator.comparing(Produto::getPreco))
+            .orElseThrow(() -> new RuntimeException("Nenhum produto encontrado"));
+
+        System.out.println(produtoMaisBarato);
 
     }
 
