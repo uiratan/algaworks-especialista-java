d3af192efe23e3e4860eb09b1694340d62b00a10
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 11:25:52 -0300

    27.2. Operação intermediária: Stream.filter

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -13,7 +13,12 @@ public class Principal {
         List<Produto> produtos = cadastroProduto.obterTodos();      
         
         Stream<Produto> stream = produtos.stream();
+
         Stream<Produto> streamComEstoque = stream.filter(Produto::temEstoque);
+
+        Stream<Produto> streamComEstoqueInativo = streamComEstoque.filter(Produto::isInativo);
+
+        
 
     }
 
