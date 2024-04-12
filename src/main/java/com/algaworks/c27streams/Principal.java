44cedbae5fc3d2842b20625a6e641ca6380002ca
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 17:37:36 -0300

    27.16. Operações de redução que retornam Optional

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -1,7 +1,9 @@
 package com.algaworks.c27streams;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalInt;
 
 import com.algaworks.c27streams.estoque.CadastroProduto;
 import com.algaworks.c27streams.estoque.Produto;
@@ -10,24 +12,17 @@ public class Principal {
 
     public static void main(String[] args) {
         CadastroProduto cadastroProduto = new CadastroProduto();
-        List<Produto> produtos = cadastroProduto.obterTodos();      
+        List<Produto> produtos = cadastroProduto.obterTodos();  
+        // List<Produto> produtos = new ArrayList<>();    
 
-        // BigDecimal valorEmEstoque = produtos.stream()
-        //     // .map(produto -> produto.getPreco().multiply(new BigDecimal(produto.getQuantidade())))
-        //     .map(Produto::getValorEstoque)
-        //     .reduce(BigDecimal.ZERO, BigDecimal::add);
-
-        BigDecimal valorEmEstoque = produtos.stream()
-            .reduce(BigDecimal.ZERO, (accumulator, produto) -> {
-                BigDecimal valorEstoqueProduto = produto.getPreco()
-                    .multiply(new BigDecimal(produto.getQuantidade()));
-                return accumulator.add(valorEstoqueProduto);
-            }, BigDecimal::add);
+        OptionalInt maiorQuantidadeOptional = produtos.stream()
+            .mapToInt(Produto::getQuantidade)
+            .reduce(Integer::max);
+            // .reduce(0, Integer::max);
         
-        // BigDecimal valorEmEstoque = produtos.stream()
-        //     .reduce(BigDecimal.ZERO, (accumulator, produto) -> accumulator.add(produto.getValorEstoque()), BigDecimal::add);
-
-        System.out.println("Valor em estoque: " + valorEmEstoque);
+        int maiorQuantidade = maiorQuantidadeOptional.orElseThrow(() -> new RuntimeException("Quantidade não encontrada"));
+        
+        System.out.println("Total em estoque: " + maiorQuantidade);
 
     }
 
