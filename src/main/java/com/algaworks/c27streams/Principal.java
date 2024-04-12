eade29611b402ff3c25d97f4f7c210050ee033c2
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 12:45:33 -0300

    27.6. Operações terminais de curto-circuito: findFirst e findAny

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -1,7 +1,7 @@
 package com.algaworks.c27streams;
 
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 
 import com.algaworks.c27streams.estoque.CadastroProduto;
 import com.algaworks.c27streams.estoque.Produto;
@@ -12,17 +12,18 @@ public class Principal {
         CadastroProduto cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();      
         
-        produtos.stream()
-            .peek(produto -> produto.setNome(produto.getNome().toUpperCase()))
-            .peek(p -> System.out.println("Antes do temEstoque: " + p))
+        Optional<Produto> produtoOptional = produtos.stream()
+            .peek(System.out::println)
             .filter(Produto::temEstoque)
-            .peek(p -> System.out.println("Depois do temEstoque: " + p))
             .filter(Produto::isInativo)
-            .forEach(produto -> {
-                System.out.println("Ativando: " + produto);
-                produto.ativar();
-            });
+            .findAny();
 
+
+        System.out.println("------------");
+        Produto produto = produtoOptional.orElseThrow(() -> new RuntimeException("Nenhum produto encontrado!"));
+
+        System.out.println(produto);
+            
     }
 
 }
