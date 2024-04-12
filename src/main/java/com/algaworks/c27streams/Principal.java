b42b10605b584179dfdabce7361c1a829d305a92
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 13:31:36 -0300

    27.10. Aplicando transformações com Stream.map

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -4,6 +4,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import com.algaworks.c27streams.estoque.CadastroProduto;
+import com.algaworks.c27streams.estoque.Fabricante;
 import com.algaworks.c27streams.estoque.Produto;
 
 public class Principal {
@@ -11,14 +12,11 @@ public class Principal {
     public static void main(String[] args) {
         CadastroProduto cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();      
-        
-        boolean temProdutoComEstoque = produtos.stream()
-            .peek(System.out::println)
-            .filter(Produto::temEstoque)
-            .sorted(Comparator.comparingInt(Produto::getQuantidade))
-            .anyMatch(Produto::temEstoque);
 
-        System.out.println(temProdutoComEstoque);
+        produtos.stream()
+            .filter(Produto::temEstoque)
+            .map(Produto::getFabricante)
+            .forEach(System.out::println);
         
     }
 
