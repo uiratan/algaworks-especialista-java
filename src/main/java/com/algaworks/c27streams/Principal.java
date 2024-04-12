74e97c1702b299b41ff81f1fee16a529915cde42
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-12 17:50:56 -0300

    27.17. Operações de redução especiais: sum, average e count

--- src/main/java/com/algaworks/c27streams/Principal.java
+++ src/main/java/com/algaworks/c27streams/Principal.java
@@ -1,8 +1,10 @@
 package com.algaworks.c27streams;
 
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
 
 import com.algaworks.c27streams.estoque.CadastroProduto;
@@ -13,16 +15,25 @@ public class Principal {
     public static void main(String[] args) {
         CadastroProduto cadastroProduto = new CadastroProduto();
         List<Produto> produtos = cadastroProduto.obterTodos();  
-        // List<Produto> produtos = new ArrayList<>();    
 
-        OptionalInt maiorQuantidadeOptional = produtos.stream()
-            .mapToInt(Produto::getQuantidade)
-            .reduce(Integer::max);
-            // .reduce(0, Integer::max);
-        
-        int maiorQuantidade = maiorQuantidadeOptional.orElseThrow(() -> new RuntimeException("Quantidade não encontrada"));
-        
-        System.out.println("Total em estoque: " + maiorQuantidade);
+        // int totalEstoque = produtos.stream()
+        //     .mapToInt(Produto::getQuantidade)
+        //     .sum();
+               
+        // System.out.println(totalEstoque);
+
+        // double mediaEstoque = produtos.stream()
+        //     .mapToInt(Produto::getQuantidade)
+        //     .average()
+        //     .orElseThrow(() -> new RuntimeException("Nenhum item encontrado"));
+           
+        // System.out.println(mediaEstoque);
+
+        long totalProdutosComEstoque = produtos.stream()
+            .filter(Produto::temEstoque)
+            .count();
+       
+        System.out.println(totalProdutosComEstoque);
 
     }
 
