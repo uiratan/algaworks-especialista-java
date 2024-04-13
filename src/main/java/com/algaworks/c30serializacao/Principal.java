045b5ac915eca60d3d16cc9c3e1b0549878bbc7b
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-13 10:59:56 -0300

    30.4. Desserializando objetos com ObjectInputStream

--- src/main/java/com/algaworks/c30serializacao/Principal.java
+++ src/main/java/com/algaworks/c30serializacao/Principal.java
@@ -1,10 +1,12 @@
 package com.algaworks.c30serializacao;
 
 import static java.nio.file.StandardOpenOption.CREATE;
+import static java.nio.file.StandardOpenOption.READ;
 import static java.nio.file.StandardOpenOption.WRITE;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.math.BigDecimal;
 import java.nio.file.Files;
@@ -15,25 +17,13 @@ import java.time.Month;
 public class Principal {
 
     public static void main(String[] args) {
-        Funcionario funcionario = new Funcionario("João", LocalDate.of(2005, Month.MARCH, 20),
-                new BigDecimal("25000"));
-
-        System.out.println(funcionario);
-
         Path path = Path.of(
                 "/mnt/wsl/PHYSICALDRIVE1/algaworks/especialista-java/src/main/java/com/algaworks/c30serializacao/objetos/joao.ser");
 
-        // try (var outputStream =
-        // new ObjectOutputStream(new FileOutputStream(path.toFile()))) {
-        // outputStream.writeObject(funcionario);
-        // } catch (IOException e) {
-        // throw new RuntimeException(e);
-        // }
-
-        try (var outputStream = new ObjectOutputStream(
-                                        Files.newOutputStream(path, WRITE, CREATE))) {
-            outputStream.writeObject(funcionario);
-        } catch (IOException e) {
+        try (var inputStream = new ObjectInputStream(Files.newInputStream(path, READ))) {
+            Funcionario funcionario = (Funcionario) inputStream.readObject();
+            System.out.println(funcionario);
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
 
