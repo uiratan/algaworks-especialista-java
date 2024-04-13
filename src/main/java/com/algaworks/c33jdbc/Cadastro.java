1054f9fea49546a83ec89befb732680f2ccbac51
Uiratan Cavalcante <uiratan@gmail.com>
2024-04-13 16:42:50 -0300

    33.11. Obtendo código de autoincremento gerado

--- src/main/java/com/algaworks/c33jdbc/Cadastro.java
+++ src/main/java/com/algaworks/c33jdbc/Cadastro.java
@@ -5,44 +5,66 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Cadastro {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Nome: ");
-        String nome = scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            String dml = """
+                        INSERT INTO venda (
+                            nome_cliente, 
+                            valor_total, 
+                            data_pagamento) 
+                        VALUES (?, ?, ?)
+                    """;
 
-        System.out.print("Valor total: ");
-        BigDecimal valorTotal = new BigDecimal(scanner.nextLine());
+            try (
+                Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/comercial", "root", "");
+                PreparedStatement comando = conexao.prepareStatement(dml, Statement.RETURN_GENERATED_KEYS);
+            ) {
 
-        System.out.print("Data de pagamento: ");
-        LocalDate dataPagamento = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                conexao.setAutoCommit(false);
 
-        String dml = """
-                    INSERT INTO venda (
-                        nome_cliente, 
-                        valor_total, 
-                        data_pagamento) 
-                    VALUES (?, ?, ?)
-                """;
+                try {
+                    do { 
+                        System.out.print("Nome: ");
+                        String nome = scanner.nextLine();
+         
+                        System.out.print("Valor total: ");
+                        BigDecimal valorTotal = new BigDecimal(scanner.nextLine());
+         
+                        System.out.print("Data de pagamento: ");
+                        LocalDate dataPagamento = LocalDate.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 
-        try (
-            Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/comercial", "root", "");
-            PreparedStatement comando = conexao.prepareStatement(dml);
-        ) {
-            comando.setString(1, nome);
-            comando.setBigDecimal(2, valorTotal);
-            comando.setDate(3, Date.valueOf(dataPagamento));
-            comando.executeUpdate();
+                        comando.setString(1, nome);
+                        comando.setBigDecimal(2, valorTotal);
+                        comando.setDate(3, Date.valueOf(dataPagamento));
+                        comando.executeUpdate();
+   
+                        ResultSet codigoResultSet = comando.getGeneratedKeys();
+                        codigoResultSet.next();
+                        long codigoGerado = codigoResultSet.getLong(1);
+   
+                        System.out.println("Venda registrada com codigo " + codigoGerado + "!");
+                        
+                        System.out.print("Continuar?");
+                    } while ("s".equalsIgnoreCase(scanner.nextLine()));
 
-            System.out.println("Venda registrada com sucesso!");
-        } catch (SQLException e) {
-            System.out.println("Erro de banco de dados");
-            e.printStackTrace();
+                    conexao.commit();
+                    
+                } catch (Exception e) {
+                    conexao.rollback();
+                    throw new RuntimeException(e);
+                }
+
+            } catch (Exception e) {
+                System.out.println("Erro de banco de dados");
+                e.printStackTrace();
+            }
         } 
 
     }
